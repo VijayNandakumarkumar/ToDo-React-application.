8c9ebbf5a30c3092869a79f18f962856eef7e705
VijayNandakumarkumar <vijay.nandakumar30@gmail.com>
2020-05-09 19:41:04 +0530

    v1.1: Adding Animation for lists.

--- totoapp/src/Listitems.js
+++ totoapp/src/Listitems.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./listIItem.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import FlipMove from "react-flip-move";
 function Listitems(props) {
   const items = props.items;
   const listItems = items.map((item) => {
@@ -26,7 +27,13 @@ function Listitems(props) {
       </div>
     );
   });
-  return <div>{listItems}</div>;
+  return (
+    <div>
+      <FlipMove duration={500} easing="ease-in-out">
+        {listItems}
+      </FlipMove>
+    </div>
+  );
 }
 
 export default Listitems;
